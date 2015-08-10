3ab836861cc7b4d998d1d88c1488d984497a783a
Chris Needham <chris.needham@bbc.co.uk>
2015-08-10 11:55:05 +0100

    Added sync to live tile

---
+++ euromeme-ui/src/js/api/sync.js
@@ -0,0 +1,63 @@
+var Promise = require('es6-promise').Promise;
+
+/**
+ * Simple wrapper for a MediaScape shared motion object (Media State Vector)
+ * @class
+ */
+
+function Sync(msv) {
+  this._msv = msv;
+}
+
+Sync.prototype.restart = function() {
+  this._msv.update(0, 1);
+}
+
+Sync.prototype.play = function() {
+  this._msv.update(null, 1);
+}
+
+Sync.prototype.pause = function() {
+  this._msv.update(null, 0);
+}
+
+/**
+ * Initialises synchronisation between an HTML media element and a
+ * MediaScape shared motion.
+ *
+ * @param {HTMLMediaElement} mediaElement A video or audio HTML element.
+ * @param {string} appId Application ID, for accessing the MCorp APIs.
+ * @param {string} msvName The name of the MediaScape shared motion to use.
+ * @param {Object} options Optional options object.
+ * @param {Boolean} options.debug If true, write debug output to the console.
+ */
+
+function init(mediaElement, appId, msvName, options) {
+  options = options || {};
+
+  return new Promise(function(resolve, reject) {
+    var app = MCorp.app(appId, { anon: true });
+
+    app.run = function() {
+      var msv = app.msvs[msvName];
+      var mediaSyncOptions = {};
+
+      if (!msv) {
+        reject(new Error('Sync initialisation failed, unknown MSV: ' + msvName));
+        return;
+      }
+
+      if (options.debug === true) {
+        mediaSyncOptions.debug = true;
+      }
+
+      app.sync = mediascape.mediaSync(mediaElement, msv, mediaSyncOptions);
+
+      resolve(new Sync(msv));
+    }
+
+    app.init();
+  });
+}
+
+module.exports = { init: init };
--- euromeme-ui/src/js/react/live-tile.js
+++ euromeme-ui/src/js/react/live-tile.js
@@ -1,6 +1,17 @@
 var React = require('react');
+var Sync = require('../api/sync');
+var configApi = require('../api/config');
 
 module.exports = React.createClass({
+  componentDidMount: function () {
+    configApi
+      .config()
+      .then(this.initSync);
+  },
+  initSync: function (config) {
+    var $video = this.refs.video.getDOMNode();
+    return Sync.init($video, config.appId, config.msvName, { debug: true });
+  },
   handleTileSelection: function () {
     var $video = this.refs.video.getDOMNode();
     $video.paused ? $video.play() : $video.pause();
