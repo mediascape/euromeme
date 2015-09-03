46ba5739a95534357e8ae45fde8e476fb12d9aa0
Dan Nuttall <pixelblend@gmail.com>
2015-09-03 10:52:03 +0100

    Add id to latest clips

--- apps/clips-api/lib/latest-clips.js
+++ apps/clips-api/lib/latest-clips.js
@@ -1,10 +1,11 @@
 var _ = require('lodash');
 
 module.exports = function(clips, count) {
-  return _(clips).reverse().slice(0,count).map(function(c) {
-    var path = '/clips/'+c+'/'+c+'.$size';
+  return _(clips).reverse().slice(0,count).map(function(clipId) {
+    var path = '/clips/'+clipId+'/'+clipId+'.$size';
 
     return  {
+      id: clipId,
       poster: path + '.jpg',
       mp4: path + '.mp4',
       gif: path + '.gif',
