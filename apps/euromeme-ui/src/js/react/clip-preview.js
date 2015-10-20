299261e3167ff3f6a7a916976b0ee5bfb9d1df82
Libby Miller <libby.miller@bbc.co.uk>
2015-10-20 13:34:08 +0100

    Smaller gifs

--- apps/euromeme-ui/src/js/react/clip-preview.js
+++ apps/euromeme-ui/src/js/react/clip-preview.js
@@ -20,7 +20,7 @@ module.exports = React.createClass({
     }
   },
   render: function() {
-    var clipUrl = this.props.clip.gif.replace('$size', 360);
+    var clipUrl = this.props.clip.gif.replace('$size', 180);
     return (
       <div className="clip-preview">
         <ActionsList
